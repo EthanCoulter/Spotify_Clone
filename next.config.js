228bb8a675b28af10b48bfd900c531941d4de6dd
Ethan Coulter <ethanscottcoulter@gmail.com>
2023-06-17 14:59:01 -0400

    restage

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            'bkkuvfxisuhluuwfmngc.supabase.co'
+        ]
+    }
+}
+
+module.exports = nextConfig
